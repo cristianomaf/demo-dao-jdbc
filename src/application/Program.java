e54fee4062ed30c24669f0bfe9c5ee8248798806
cristianomaf <cristianomaf@live.com>
2019-09-21 10:17:07 -0300

    Classe Vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
 		Departamento obj = new Departamento(1,"Livros");
-		System.out.println(obj);
+		Vendedor vendedor = new Vendedor(21,"Bob","bob@gmail.com",new Date(),3000.0,obj);
+		System.out.println(vendedor);
 
 	}
 
