415a5d7730158d60c20127001c418a56c5379e97
cristianomaf <cristianomaf@live.com>
2019-09-22 09:24:38 -0300

    VendedorDAOJDBC e DAOFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.VendedorDao;
 import model.entities.Departamento;
 import model.entities.Vendedor;
 
@@ -11,6 +13,9 @@ public class Program {
 	
 		Departamento obj = new Departamento(1,"Livros");
 		Vendedor vendedor = new Vendedor(21,"Bob","bob@gmail.com",new Date(),3000.0,obj);
+	
+		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
+		
 		System.out.println(vendedor);
 
 	}
