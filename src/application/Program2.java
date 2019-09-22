ef7c57a64726caa24518d1ae4db22cb03fc3292e
cristianomaf <cristianomaf@live.com>
2019-09-22 12:50:06 -0300

    Final com todas as operacoes com Departamento e program 2

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.List;
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.DepartamentoDao;
 import model.entities.Departamento;
@@ -7,6 +10,7 @@ import model.entities.Departamento;
 public class Program2 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		DepartamentoDao departmentDao = DaoFactory.createDepartmentDao();
 
 		System.out.println("\n ===TESTE 1: Departamento INSERIR===");
@@ -15,6 +19,31 @@ public class Program2 {
 		departmentDao.insert(novoDp);
 		System.out.println("Inserido! New id; " + novoDp.getId());
 
+		System.out.println("\n ===TESTE 2: Departamento UPDATE ===");
+		Departamento novoDp2 = departmentDao.findById(8);
+		novoDp2.setNome("Casa");
+		departmentDao.update(novoDp2);
+		System.out.println("Update Realizado");
+
+		System.out.println("=== TESTE 3: findById =======");
+		Departamento dep = departmentDao.findById(1);
+		System.out.println(dep);
+
+		System.out.println("\n=== TESTE 4: findAll =======");
+		List<Departamento> list = departmentDao.findAll();
+		for (Departamento d : list) {
+		System.out.println(d);
+		}
+
+			System.out.println("\n=== TEST 5: delete =======");
+			System.out.print("Enter id for delete test: ");
+			int id = sc.nextInt();
+			departmentDao.deleteById(id);
+			System.out.println("Delete completed");
+			
+			sc.close();
+
+		
 	}
 
 }
--- src/model/dao/impl/DepartamentoDaoJDBC.java
+++ src/model/dao/impl/DepartamentoDaoJDBC.java
@@ -4,12 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.mysql.jdbc.Statement;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartamentoDao;
 import model.entities.Departamento;
 
@@ -59,26 +61,100 @@ public class DepartamentoDaoJDBC implements DepartamentoDao {
 
 	@Override
 	public void update(Departamento obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("UPDATE department "
+					+ "SET Name = ? "
+					+  "WHERE Id = ?");
+
+			st.setString(1, obj.getNome());
+			st.setInt(2, obj.getId());
+			
+			st.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+
+		}
 		
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+				"DELETE FROM department WHERE Id = ?");
+
+			st.setInt(1, id);
+
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
 	public Departamento findById(Integer Id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+				"SELECT * FROM department WHERE Id = ?");
+			st.setInt(1, Id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Departamento obj = new Departamento();
+				obj.setId(rs.getInt("Id"));
+				obj.setNome(rs.getString("Name"));
+				return obj;
+			}
+			return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
 	}
 
 	@Override
 	public List<Departamento> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+				"SELECT * FROM department ORDER BY Name");
+			rs = st.executeQuery();
+
+			List<Departamento> list = new ArrayList<>();
+
+			while (rs.next()) {
+				Departamento obj = new Departamento();
+				obj.setId(rs.getInt("Id"));
+				obj.setNome(rs.getString("Name"));
+				list.add(obj);
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	
 	}
 	
 
